ef0a4e07d96a2a7c895e8655087185fbe9967cb9
Andrew Kane <acekane1@gmail.com>
2014-05-10 18:54:59 -0700

    Use ActionMailer::Base.default_url_options

--- app/helpers/mailkick/url_helper.rb
+++ app/helpers/mailkick/url_helper.rb
@@ -3,7 +3,7 @@ module Mailkick
 
     def mailkick_unsubscribe_url
       Mailkick::Engine.routes.url_helpers.url_for(
-        Rails.application.config.action_mailer.default_url_options.merge(
+        (ActionMailer::Base.default_url_options || {}).merge(
           controller: "mailkick/subscriptions",
           action: "unsubscribe",
           id: "{{MAILKICK_TOKEN}}"
