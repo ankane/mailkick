5c7d13892b5d1d22ea5de1c28f44830de2f11906
Andrew Kane <acekane1@gmail.com>
2015-01-31 13:31:23 -0800

    Only set secret token if not already set

--- lib/mailkick/engine.rb
+++ lib/mailkick/engine.rb
@@ -5,7 +5,7 @@ module Mailkick
     initializer "mailkick" do |app|
       Mailkick.discover_services
       secrets = app.respond_to?(:secrets) ? app.secrets : app.config
-      Mailkick.secret_token = secrets.respond_to?(:secret_key_base) ? secrets.secret_key_base : secrets.secret_token
+      Mailkick.secret_token ||= secrets.respond_to?(:secret_key_base) ? secrets.secret_key_base : secrets.secret_token
       ActiveSupport.on_load :action_mailer do
         helper Mailkick::UrlHelper
       end
