3a222615543b300b4fa7cf467f0757b9814d637c
Stefan Wrobel <swrobel@users.noreply.github.com>
2016-08-23 13:58:27 -0700

    Don't discover services if they've been manually initialized

--- lib/mailkick.rb
+++ lib/mailkick.rb
@@ -22,7 +22,7 @@ module Mailkick
 
   def self.discover_services
     Service.subclasses.each do |service|
-      services << service.new if service.discoverable?
+      services << service.new if service.discoverable? && !services.map(&:class).include?(service)
     end
   end
 
