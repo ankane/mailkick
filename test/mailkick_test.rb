74fe34d687c5342fcc8961c3bb2931624e50d2e1
Andrew Kane <acekane1@gmail.com>
2017-01-07 23:19:27 -0800

    Added html test

--- test/mailkick_test.rb
+++ test/mailkick_test.rb
@@ -3,7 +3,9 @@ require_relative "test_helper"
 class MailkickTest < Minitest::Test
   def test_unsubscribe_url
     message = UserMailer.welcome.deliver_now
-    body = message.body.to_s
-    assert_match "Boom", body
+    html_body = message.html_part.body.to_s
+    assert_equal "<p>BAhbCUkiFXRlc3RAZXhhbXBsZS5vcmcGOgZFVDAwMA%3D%3D--f435e91ba90e1732d3e999af1f2126dcc8182a5d</p>", html_body
+    text_body = message.text_part.body.to_s
+    assert_equal "Boom: BAhbCUkiFXRlc3RAZXhhbXBsZS5vcmcGOgZFVDAwMA%3D%3D--f435e91ba90e1732d3e999af1f2126dcc8182a5d", text_body
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,6 +14,9 @@ class UserMailer < ActionMailer::Base
   default from: "from@example.com"
 
   def welcome
-    mail to: "test@example.org", subject: "Hello", body: "Boom subscriptions/%7B%7BMAILKICK_TOKEN%7D%7D/unsubcribe"
+    mail to: "test@example.org", subject: "Hello" do |format|
+      format.html { render text: "<p>%7B%7BMAILKICK_TOKEN%7D%7D</p>" }
+      format.text { render text: "Boom: %7B%7BMAILKICK_TOKEN%7D%7D" }
+    end
   end
 end
