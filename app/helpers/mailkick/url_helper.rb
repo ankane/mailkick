d6b6796203853eaf69a7a40826283138577d42cc
Andrew <acekane1@gmail.com>
2018-04-20 01:25:33 -0700

    Cache url

--- app/helpers/mailkick/url_helper.rb
+++ app/helpers/mailkick/url_helper.rb
@@ -1,13 +1,15 @@
 module Mailkick
   module UrlHelper
     def mailkick_unsubscribe_url
-      Mailkick::Engine.routes.url_helpers.url_for(
-        (ActionMailer::Base.default_url_options || {}).merge(
-          controller: "mailkick/subscriptions",
-          action: "unsubscribe",
-          id: "{{MAILKICK_TOKEN}}"
+      @mailkick_unsubscribe_url ||= begin
+        Mailkick::Engine.routes.url_helpers.url_for(
+          (ActionMailer::Base.default_url_options || {}).merge(
+            controller: "mailkick/subscriptions",
+            action: "unsubscribe",
+            id: "{{MAILKICK_TOKEN}}"
+          )
         )
-      )
+      end
     end
   end
 end
