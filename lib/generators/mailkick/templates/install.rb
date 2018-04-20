d7ca6673a11daa9ab64710f17bdc75029fc3007e
Andrew <acekane1@gmail.com>
2018-04-20 01:41:27 -0700

    Fixed migration

--- lib/generators/mailkick/templates/install.rb
+++ lib/generators/mailkick/templates/install.rb
@@ -2,7 +2,7 @@ class <%= migration_class_name %> < ActiveRecord::Migration<%= migration_version
   def change
     create_table :mailkick_opt_outs do |t|
       t.string :email
-      t.references :user
+      t.references :user, polymorphic: true
       t.boolean :active, null: false, default: true
       t.string :reason
       t.string :list
