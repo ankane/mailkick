306cd8fdf27df6950deaeea16b16a4bf9a23548e
Andrew <acekane1@gmail.com>
2018-04-20 04:05:34 -0700

    Gracefully handle missing email

--- app/helpers/mailkick/url_helper.rb
+++ app/helpers/mailkick/url_helper.rb
@@ -1,7 +1,10 @@
 module Mailkick
   module UrlHelper
     def mailkick_unsubscribe_url(email: nil, list: nil, **options)
-      email ||= controller.message.to.first
+      email ||= controller.try(:message).try(:to).try(:first)
+
+      raise ArgumentError, "Missing email" unless email
+
       Mailkick::Engine.routes.url_helpers.url_for(
         (ActionMailer::Base.default_url_options || {}).merge(options).merge(
           controller: "mailkick/subscriptions",
