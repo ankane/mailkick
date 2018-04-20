1a038b82c31b51cb9d7a0c5c0a6c92d3e76dd938
Andrew <acekane1@gmail.com>
2018-04-20 03:32:27 -0700

    Support url options

--- app/helpers/mailkick/url_helper.rb
+++ app/helpers/mailkick/url_helper.rb
@@ -1,9 +1,9 @@
 module Mailkick
   module UrlHelper
-    def mailkick_unsubscribe_url(email: nil, list: nil)
+    def mailkick_unsubscribe_url(email: nil, list: nil, **options)
       email ||= controller.message.to.first
       Mailkick::Engine.routes.url_helpers.url_for(
-        (ActionMailer::Base.default_url_options || {}).merge(
+        (ActionMailer::Base.default_url_options || {}).merge(options).merge(
           controller: "mailkick/subscriptions",
           action: "unsubscribe",
           id: Mailkick.generate_token(email, list: list)
