911e80f879ffb7b7bf2ad9a8fa8ba26f01c4235f
Andrew <acekane1@gmail.com>
2018-04-20 02:34:40 -0700

    Fixed secret_token for Rails 5.2

--- lib/mailkick/engine.rb
+++ lib/mailkick/engine.rb
@@ -5,10 +5,17 @@ module Mailkick
     initializer "mailkick" do |app|
       Mailkick.discover_services unless Mailkick.services.any?
 
-      # default to secrets to keep backward compatible
-      ActiveSupport::Deprecation.silence do
-        secrets = app.respond_to?(:secrets) ? app.secrets : app.config
-        Mailkick.secret_token ||= secrets.respond_to?(:secret_key_base) ? secrets.secret_key_base : secrets.secret_token
+      Mailkick.secret_token ||= begin
+        creds =
+          if app.respond_to?(:credentials)
+            app.credentials
+          elsif app.respond_to?(:secrets)
+            app.secrets
+          else
+            app.config
+          end
+
+        creds.respond_to?(:secret_key_base) ? creds.secret_key_base : creds.secret_token
       end
 
       ActiveSupport.on_load :action_mailer do
