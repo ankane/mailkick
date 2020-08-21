8c2855ff9f9f9b4dc55a3bc0745151574d0e5af2
Andrew Kane <andrew@chartkick.com>
2020-08-21 03:45:12 -0700

    Added controller test

---
+++ test/controller_test.rb
@@ -0,0 +1,18 @@
+require_relative "test_helper"
+
+class ControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    Mailkick::OptOut.delete_all
+  end
+
+  def test_unsubscribe_url
+    message = UserMailer.welcome.deliver_now
+    text_body = message.text_part.body.to_s
+    url = /Unsubscribe: (.+)/.match(text_body)[1]
+
+    get url
+    assert_response :redirect
+
+    assert_equal 1, Mailkick::OptOut.count
+  end
+end
