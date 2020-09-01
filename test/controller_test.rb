73f7476fd390e3861852bfa17e2138a00002e4d1
Andrew Kane <andrew@chartkick.com>
2020-09-01 03:08:44 -0700

    Better test

--- test/controller_test.rb
+++ test/controller_test.rb
@@ -15,12 +15,12 @@ class ControllerTest < ActionDispatch::IntegrationTest
     assert_equal 1, Mailkick::OptOut.count
   end
 
-  def test_bad_request
+  def test_bad_signature
     message = UserMailer.welcome.deliver_now
     text_body = message.text_part.body.to_s
     url = /Unsubscribe: (.+)/.match(text_body)[1]
 
-    get url.sub("subscriptions/", "subscriptions/a")
+    get url.sub("/unsubscribe", "bad/unsubscribe")
     assert_response :bad_request
     assert_equal "Subscription not found", response.body
   end
