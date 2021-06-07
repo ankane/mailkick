67dcd5848efe3a2cda60def7b3d1b93acda1bbe5
Andrew Kane <andrew@ankane.org>
2021-06-07 14:32:39 -0700

    Added test for process opt-outs

--- test/service_test.rb
+++ test/service_test.rb
@@ -8,11 +8,39 @@ end
 
 class ServiceTest < Minitest::Test
   def setup
+    super
     Mailkick.services = [TestService.new]
   end
 
   def teardown
+    super
     Mailkick.services = []
+  end
+
+  def test_process_opt_outs
+    user = User.create!(email: "test@example.com")
+    user.subscribe("sales")
+
+    previous_method = Mailkick.process_opt_outs_method
+    begin
+      Mailkick.process_opt_outs_method = lambda do |opt_outs|
+        emails = opt_outs.map { |v| v[:email] }
+        subscribers = User.includes(:mailkick_subscriptions).where(email: emails).index_by(&:email)
+
+        opt_outs.each do |opt_out|
+          subscriber = subscribers[opt_out[:email]]
+          next unless subscriber
+
+          subscriber.mailkick_subscriptions.each do |subscription|
+            subscription.destroy if subscription.created_at < opt_out[:time]
+          end
+        end
+      end
+      Mailkick.fetch_opt_outs
+      refute user.subscribed?("sales")
+    ensure
+      Mailkick.process_opt_outs_method = previous_method
+    end
   end
 
   def test_not_configured
