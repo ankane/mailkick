63bbc01af46a2bc3e2656e002ca1bedaf80eba61
Andrew Kane <andrew@ankane.org>
2024-09-08 19:00:38 -0700

    Fixed deprecation warning with Rails 7.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.3.1 (unreleased)
+
+- Fixed deprecation warning with Rails 7.1
+
 ## 1.3.0 (2024-01-18)
 
 - Added support for one-click unsubscribe headers (RFC 8058)
--- lib/mailkick/engine.rb
+++ lib/mailkick/engine.rb
@@ -13,7 +13,7 @@ module Mailkick
         creds =
           if app.respond_to?(:credentials) && app.credentials.secret_key_base
             app.credentials
-          elsif app.respond_to?(:secrets)
+          elsif app.respond_to?(:secrets) && app.config.paths["config/secrets"].existent.any?
             app.secrets
           else
             app.config
