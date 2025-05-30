4ff49c54f198f032a943477e4d0369cb3cdc0e70
Andrew Kane <andrew@ankane.org>
2025-05-30 00:34:42 -0700

    Improved code [skip ci]

--- lib/generators/mailkick/views_generator.rb
+++ lib/generators/mailkick/views_generator.rb
@@ -3,7 +3,7 @@ require "rails/generators"
 module Mailkick
   module Generators
     class ViewsGenerator < Rails::Generators::Base
-      source_root File.expand_path("../../../../app/views", __FILE__)
+      source_root File.expand_path("../../../app/views", __dir__)
 
       def copy_initializer_file
         directory "mailkick", "app/views/mailkick"
