16a0fa0ab062b5b56d6ab54687b3767ba8d8c833
Andrew Kane <andrew@ankane.org>
2025-10-21 20:04:33 -0700

    Updated test setup [skip ci]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,6 @@ require "bundler/setup"
 require "combustion"
 Bundler.require(:default)
 require "minitest/autorun"
-require "minitest/pride"
 
 Combustion.path = "test/internal"
 Combustion.initialize! :active_record, :action_mailer do
